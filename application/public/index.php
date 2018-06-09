d8feb338abd69a04e61fce188c84988090b18fea
yivi <ivan@yivoff.com>
2018-06-09 19:53:41 +0200

    - Define a constant with application root path, to use in the whole app
    - Require configuration files in the entry point. If they are missing,
      we shoulnd't continue anyway.

--- application/public/index.php
+++ application/public/index.php
@@ -1,46 +1,43 @@
 <?php
 
 // Setup configuration files
-$appDir = dirname(__DIR__);
-$srcDir = $appDir . '/src';
+define( 'APP_ROOT_PATH', dirname( __DIR__ ) );
+$config   = APP_ROOT_PATH . '/config/config.php';
+$autoLoad = APP_ROOT_PATH . '/config/autoload.php';
+$routes   = APP_ROOT_PATH . '/config/routes.php';
 
-require $srcDir . '/library/interfaces/IRun.php';
-require $srcDir . '/library/application/Micro.php';
-
-$configPath = $appDir . '/config/';
-$config = $configPath . 'config.php';
-$autoLoad = $configPath . 'autoload.php';
-$routes = $configPath . 'routes.php';
+require APP_ROOT_PATH . '/src/library/interfaces/IRun.php';
+require APP_ROOT_PATH . '/src/library/application/Micro.php';
 
 try {
-    $app = new Minube\Application\Micro();
+	$app = new Minube\Application\Micro();
 
-    $app->setAutoload($autoLoad, $srcDir);
-    $app->setConfig($config);
+	$app->setAutoload( require $autoLoad );
+	$app->setConfig( require $config );
 
-    switch ($_SERVER['REQUEST_METHOD']) {
+	switch ( $_SERVER['REQUEST_METHOD'] ) {
 
-        case 'GET':
-            $data = $_GET;
-            unset($data['_url']);
-            break;
+		case 'GET':
+			$data = $_GET;
+			unset( $data['_url'] );
+			break;
 
-        case 'POST':
-            $data = $_POST;
-            break;
+		case 'POST':
+			$data = $_POST;
+			break;
 
-        default: // PUT AND DELETE
-            parse_str(file_get_contents('php://input'), $data);
-            break;
-    }
+		default: // PUT AND DELETE
+			parse_str( file_get_contents( 'php://input' ), $data );
+			break;
+	}
+	$app->setRoutes( require $routes );
 
-    $app->setRoutes($routes);
-    $app->setDependencies();
+	$app->setDependencies();
 
-    $app->run();
+	$app->run();
 
-} catch(Exception $e) {
-    $app->response->setStatusCode(500, "Server Error");
-    $app->response->setContent($e->getMessage());
-    $app->response->send();
+} catch ( Exception $e ) {
+	$app->response->setStatusCode( 500, "Server Error" );
+	$app->response->setContent( $e->getMessage() );
+	$app->response->send();
 }
