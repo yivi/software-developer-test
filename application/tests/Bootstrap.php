d31bcd6f6e1f27cf2132d2b2828a940e27ac6cf7
yivi <ivan@yivoff.com>
2018-06-10 11:21:18 +0200

    - Fixed path references so that tests are runable

--- application/tests/Bootstrap.php
+++ application/tests/Bootstrap.php
@@ -1,47 +1,42 @@
 <?php
+namespace MinubeTests;
+
 use Phalcon\DI;
 use Phalcon\DI\FactoryDefault;
 
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+define( 'APP_ROOT_PATH', dirname( __DIR__ ) );
+ini_set( 'display_errors', 1 );
+error_reporting( E_ALL );
 
 set_include_path(
-    __DIR__ . PATH_SEPARATOR . get_include_path()
+	__DIR__ . PATH_SEPARATOR . get_include_path()
 );
 
 // required for phalcon/incubator
-include __DIR__ . "/../../vendor/autoload.php";
+include APP_ROOT_PATH . "/vendor/autoload.php";
 
 // use the application autoloader to autoload the classes
 // autoload the dependencies found in composer
 $loader = new \Phalcon\Loader();
 
-$appDir = dirname(__DIR__);
-$srcDir = $appDir . '/../src';
-
-$namespaces = array(
-    'MinubeTests' => __DIR__ . '/tests/',
-    'Minube\Application' => $srcDir . '/library/application/',
-    'Minube\Interfaces' => $srcDir . '/library/interfaces/',
-    'Minube\Controllers' => $srcDir . '/controllers/',
-    'Minube\Models' => $srcDir . '/models/',
-);
+$namespaces = [
+	'MinubeTests'        => __DIR__,
+	'Minube\Application' => APP_ROOT_PATH . '/src/library/application/',
+	'Minube\Interfaces'  => APP_ROOT_PATH . '/src/library/interfaces/',
+	'Minube\Controllers' => APP_ROOT_PATH . '/src/controllers/',
+	'Minube\Models'      => APP_ROOT_PATH . '/src/models/',
+];
 
 // get namespaces from Composer
-$map = require __DIR__ . '/../../vendor/composer/autoload_namespaces.php';
+$map = require __DIR__ . '/../vendor/composer/autoload_namespaces.php';
 
-foreach ($map as $namespace => $values) {
-    $namespace = trim($namespace, '\\');
-    if (!isset($namespaces[$namespace])) {
-        $namespaces[$namespace] = $values[0];
-    }
+foreach ( $map as $namespace => $values ) {
+	$namespace = trim( $namespace, '\\' );
+	if ( ! isset( $namespaces[ $namespace ] ) ) {
+		$namespaces[ $namespace ] = $values[0];
+	}
 }
-$loader->registerNamespaces($namespaces);
-
-$loader->registerDirs(array(
-    __DIR__ . '/../src/',
-    __DIR__ . '/tests/',
-));
+$loader->registerNamespaces( $namespaces );
 
 $loader->register();
 
@@ -50,4 +45,4 @@ DI::reset();
 
 // add any needed services to the DI here
 
-DI::setDefault($di);
+DI::setDefault( $di );
